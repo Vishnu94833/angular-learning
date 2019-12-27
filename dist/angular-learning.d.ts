bc5170734c92e4cc1bd8897f753465a829d13166
Vishnu94833 <vishnu94833@gmail.com>
2019-12-27 18:43:13 +0530

    removed dist

--- dist/angular-learning.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-export * from './public_api';
-export { DropdownComponent as ɵb } from './src/app/modules/dropdown/dropdown.component';
-export { HeaderComponent as ɵa } from './src/app/modules/header/header.component';
--- dist/src/app/modules/dropdown/dropdown.component.d.ts
+++
@@ -1,7 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class DropdownComponent implements OnInit {
-    dropdownList: any;
-    ngClass: any;
-    constructor();
-    ngOnInit(): void;
-}
